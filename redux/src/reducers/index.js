84e368a5b6fe046fbf492cf9e6d25ce4a8e70b07
Sultan1901 <sultan.aqeel.elq.js@tuwaiq.edu.sa>
2021-12-06 19:33:53 +0300

    try with It

--- redux/src/reducers/index.js
+++ redux/src/reducers/index.js
@@ -1,3 +1,8 @@
 import {creatStore , combineReducers } from "redux"
 import { composeWithDevTools } from "redux-devtools-extension"
-import {}
+import sign from "./login"
+const reducers = combineReducers({ sign })
+const store = () => {
+    return createStore(reducers , composeWithDevTools())
+}
+export default store()
